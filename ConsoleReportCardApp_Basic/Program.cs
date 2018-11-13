d1e0ac3156c06580448a9922b0eedd6eccae611d
Kenneth Bonte <kwbonte@gmail.com>
2018-11-12 19:24:44 -0600

    Add project files.

---
+++ ConsoleReportCardApp_Basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReportCardApp_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy world");
+            Console.ReadKey();
+        }
+    }
+}
